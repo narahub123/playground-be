7b277d693afe75471d8dd9d204f0828e3f236138
nara <parknara0213@gmail.com>
2024-09-30 15:55:05 +0900

    checkExistingEmail API

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,6 +1,14 @@
 import express from "express";
 import { User } from "@models";
 
-const userServices = {};
+const userServices = {
+  getUserByEamil: async (email: string) => {
+    try {
+      return await User.findOne({ email });
+    } catch (error: any) {
+      console.log("getUserByEmail에서 에러", error.errors);
+    }
+  },
+};
 
 export default userServices;
